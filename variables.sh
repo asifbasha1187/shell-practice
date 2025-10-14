99fdb294107e3aff0f59451f7bf46a42c470f685
asif basha shaik <info@devops.com>
2025-10-14 23:16:11 +0530

    some message

---
+++ variables.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+#No space before or after equal
+PERSON1=RAMESH
+PERSON2=SURESH
+
+echo "$PERSON1:: HI $PERSON2, HOW R U"
+echo "$PERSON2:: HI $PERSON1, I AM FINE, WHAT ARE U DOING"
+echo "$PERSON1:: I am learning shell scripting and GIT"
+echo "$PERSON2:: WHAT YOU LEANRT $PERSON1"
