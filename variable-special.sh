b4e1b051d7f88b4caad9f4890e27ded6fda17726
asif basha shaik <info@devops.com>
2025-10-15 23:13:48 +0530

    variable mask

---
+++ variable-special.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+echo "All variable passed to the script: $@"
+echo "All variable passed to the script: $*"
+echo "Script Name is: $0"
+echo "Current Directory: $PWD"
+echo "who is running this: $USER"
+echo "Home directory of user: $HOME"
+echo "PID of the script: $$"
+sleep 50 &
+echo PID of the last command in background is: $!"
