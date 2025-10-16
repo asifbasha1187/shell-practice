6cf138b01a731365d5c046e35aafcf22ff841c38
asif basha shaik <info@devops.com>
2025-10-16 10:46:11 +0530

    conditions

--- 01-conditions.sh
+++ 01-conditions.sh
@@ -2,7 +2,7 @@
 
 NUMBER=$1
 
-if [$NUMBER -lt 10]; then
+if [ $NUMBER -lt 10 ]; then
     echo "Given Number $NUMBER IS LESS THAN 10"
 else
     echo "Give number $NUMBER IS greathan or equal to 10"
