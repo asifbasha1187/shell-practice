b233ef7368e32747a7e0a70418e6d1f63029702c
asif basha shaik <info@devops.com>
2025-10-16 09:40:28 +0530

    data-type

--- 01-data-types.sh
+++ 01-data-types.sh
@@ -8,3 +8,9 @@ NAME=BASHA
 SUM=$(($NUMBER1+$NUMBER2+$NAME))
 echo "SUM is: ${SUM}"
 
+#ARRAY - SIZE=4, MAX INDEX=3
+LEADERS=("BASHA" "ASIF" "SHAIK" "bash")
+
+echo "All leaders: ${LEADERS[@]}"
+#echo "First leade: ${LEADERS[0]}"
+#echo "First Leader: ${LEADERS[10]}"
