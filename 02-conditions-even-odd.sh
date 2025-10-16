2e730aa59f6bc417ac7700e1197eaf7f3f0a4cf3
asif basha shaik <info@devops.com>
2025-10-16 11:07:46 +0530

    conditions

---
+++ 02-conditions-even-odd.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+echo "Please enter the number:"
+read NUMBER
+
+if [ $(($NUMBER % 2)) -eq 0]; then
+    echo "Given Number $NUMBER is EVEN"
+else
+    echo "Given Number $NUMBER is ODD"
+fi
